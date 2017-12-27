7325e825a17fe9aa83cb0dcb1d8f151578e8cfc4
hacker1024 <joshleivenzon@outlook.com>
2017-12-27 19:30:38 +0000

    Remove unused import

--- app/src/main/java/tk/superl2/ctroller/MainActivity.kt
+++ app/src/main/java/tk/superl2/ctroller/MainActivity.kt
@@ -10,7 +10,6 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import eu.chainfire.libsuperuser.Shell
-import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
